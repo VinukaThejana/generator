25150100cf02c7eb1f513d4fe7c6b5d05a14cdda
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-05-01 18:49:50 +0530

    Added some test

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,18 +2,26 @@ package utils
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
 
-// TestGetRandom is a function that is used for testing the getRandom function
+// TestGetRandomInt is a function that is used for testing the getRandom function
 // to test wether the random number is returned within the given range
-func TestGetRandom(t *testing.T) {
-	randomNum := getRandom(0, 10)
+func TestGetRandomInt(t *testing.T) {
+	randomNum := getRandomInt(0, 10)
 	fmt.Println("The random number is : ", randomNum)
 
 	if randomNum < 0 || randomNum > 10 {
 		t.Errorf("getRandom() returned a number outside the expected range: %d", randomNum)
 	}
+}
+
+// TestGetRandomLetter is a function that is used to test the function that is used to
+// generate a random letter
+func TestGetRandomLetter(t *testing.T) {
+	randomLetter := getRandomLetter()
+	fmt.Println("The random letter is : ", randomLetter)
 }
 
 // TestRandomizeWithSymbols is a function that is used to test strings inorder
@@ -30,5 +38,41 @@ func TestRandomizeWithSymbols(t *testing.T) {
 	for n, test := range tests {
 		value := randomizeWithSymbols(test)
 		fmt.Printf("\t%d). Input :  %s\n\t    Output : %s\n\n", n+1, test, value)
+	}
+}
+
+// TestGenerate is a function that is used ot check wether passowrds are generated properly
+// with the given conditions
+func TestGenerate(t *testing.T) {
+	p := Passwords{}
+
+	tests := []int{0, 12, 48, 47, 32, 33, 99, 120}
+	for _, test := range tests {
+		var password string
+		expectedLength := 32
+
+		if test == 0 {
+			pswd, err := p.Generate(nil)
+			if err != nil {
+				log.Println(err)
+				t.Error("Failed to generate password")
+				return
+			}
+			password = *pswd
+		} else {
+			expectedLength = test
+			pswd, err := p.Generate(&test)
+			if err != nil {
+				log.Println(err)
+				t.Error("Failed to generate the password")
+				return
+			}
+			password = *pswd
+		}
+
+		if len(password) != expectedLength {
+			t.Error("The password does not contain enough length")
+			return
+		}
 	}
 }
