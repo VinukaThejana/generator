5dc53ff03e3851f9fce254a7b82e8b5c3591fef3
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-05-02 20:21:12 +0530

    Added the main function

---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2023 Vinuka Kodituwakku <vinuka.t@pm.me>
+ */
+
+// Generate passwords given on the provided conditions
+// using openssl
+package main
+
+import "github.com/VinukaThejana/generator/cmd"
+
+func main() {
+	cmd.Execute()
+}
